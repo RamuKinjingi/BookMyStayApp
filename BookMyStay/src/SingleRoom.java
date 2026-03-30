586c6dbe028d7f5d0b17487970fda975846f36be
Ram <ramukinjingi@gmail.com>
2026-03-30 09:03:19 +0530

    added features of uc2

---
+++ BookMyStay/src/SingleRoom.java
@@ -0,0 +1,9 @@
+public class SingleRoom extends Room {
+    public SingleRoom() {
+        super(1, 200, 1500);
+    }
+
+    public String getRoomType() {
+        return "Single Room";
+    }
+}
--- BookMyStay/src/UC1.java
+++
@@ -1,8 +0,0 @@
-public class UC1 {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Book My Stay App!");
-        System.out.println("Application: Hotel Booking System");
-        System.out.println("Version: v1.0");
-        System.out.println("Thank you for using the application.");
-    }
-}

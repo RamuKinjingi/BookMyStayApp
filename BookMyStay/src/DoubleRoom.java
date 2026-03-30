25cb001d8507f9bdcbe66d407c834a1c3f9cef99
Ram <ramukinjingi@gmail.com>
2026-03-30 09:59:16 +0530

    Removed unused classes

--- BookMyStay/src/DoubleRoom.java
+++
@@ -1,9 +0,0 @@
-public class DoubleRoom extends Room {
-    public DoubleRoom() {
-        super(2, 350, 2500);
-    }
-
-    public String getRoomType() {
-        return "Double Room";
-    }
-}
--- BookMyStay/src/SuiteRoom.java
+++
@@ -1,9 +0,0 @@
-public class SuiteRoom extends Room {
-    public SuiteRoom() {
-        super(3, 600, 5000);
-    }
-
-    public String getRoomType() {
-        return "Suite Room";
-    }
-}
